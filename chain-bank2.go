7d142ed45c40dc89f6defdfd7ada041605e1e632
saan099 <saan099@gmail.com>
2017-04-19 16:05:36 +0530

    checking threads

--- chain-bank2.go
+++ chain-bank2.go
@@ -1,23 +1,23 @@
-
 package main
 
 import (
+	"encoding/json"
 	"errors"
-	"encoding/json"	
-	"strconv"
 	"fmt"
+	"strconv"
+	"sync"
+	"time"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-
-type account struct{
-Bank_ID string `json:"bank_ID"`
-Balance int `json:"balance"`
-Name string `json:"name"`
+type account struct {
+	Bank_ID string `json:"bank_ID"`
+	Balance int    `json:"balance"`
+	Name    string `json:"name"`
 }
 
-var indexes string="index"
-
+var indexes string = "index"
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
@@ -37,8 +37,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 
 	var empty []string
-	indexAsbytes, _:= json.Marshal(empty)
-	err := stub.PutState(indexes,indexAsbytes)
+	indexAsbytes, _ := json.Marshal(empty)
+	err := stub.PutState(indexes, indexAsbytes)
 
 	if err != nil {
 		return nil, err
@@ -57,15 +57,16 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	} else if function == "make_account" {
 		return t.make_account(stub, args)
 	} else if function == "deposit" {
-		return t.deposit(stub,args)
+		return t.deposit(stub, args)
 	} else if function == "withdrawal" {
-		return t.withdrawal(stub,args)
+		return t.withdrawal(stub, args)
+	} else if function == "work" {
+		return t.work(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
-
 
 // write - invoke function to write key/value pair
 func (t *SimpleChaincode) make_account(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
@@ -76,9 +77,8 @@ func (t *SimpleChaincode) make_account(stub shim.ChaincodeStubInterface, args []
 	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 3. name of the key and value to set")
 	}
-	
 
-	str:=`{"bank_ID": "`+args[0]+`", "balance": `+args[1]+`, "name": "`+args[2]+`"}`
+	str := `{"bank_ID": "` + args[0] + `", "balance": ` + args[1] + `, "name": "` + args[2] + `"}`
 	var index []string
 
 	key = args[0] //rename for funsies
@@ -87,14 +87,13 @@ func (t *SimpleChaincode) make_account(stub shim.ChaincodeStubInterface, args []
 	if err != nil {
 		return nil, err
 	}
-	valAsbytes,err:=stub.GetState(indexes)
-	json.Unmarshal(valAsbytes,&index)
-	index=append(index,args[0])
-	indexAsbytes,_:=json.Marshal(index)
-	err=stub.PutState(indexes,indexAsbytes)
+	valAsbytes, err := stub.GetState(indexes)
+	json.Unmarshal(valAsbytes, &index)
+	index = append(index, args[0])
+	indexAsbytes, _ := json.Marshal(index)
+	err = stub.PutState(indexes, indexAsbytes)
 	return nil, nil
 }
-
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
@@ -103,8 +102,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		return t.read(stub, args)
-	}else if function=="seeAll" {
-		return t.seeAll(stub,args)
+	} else if function == "seeAll" {
+		return t.seeAll(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
@@ -130,7 +129,23 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	return valAsbytes, nil
 }
 
-func (t *SimpleChaincode) deposit(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
+func (t *SimpleChaincode) thread(stub shim.ChaincodeStubInterface, key string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(10 * time.Second)
+	_ = stub.PutState(key, []byte("love"))
+}
+
+func (t *SimpleChaincode) work(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var wg sync.WaitGroup
+	_ = stub.PutState(args[0])
+
+	go t.thread(stub, args[1], &wg)
+	wg.Wait()
+	_ = stub.PutState(args[2], []byte("you"))
+	return nil, nil
+}
+
+func (t *SimpleChaincode) deposit(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
 
@@ -144,18 +159,17 @@ func (t *SimpleChaincode) deposit(stub shim.ChaincodeStubInterface, args []strin
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	acc:=account{}
-        json.Unmarshal(valAsbytes,&acc)
-        fmt.Println(acc)
-	num, err:=strconv.Atoi(args[1])
-	
-	
+	acc := account{}
+	json.Unmarshal(valAsbytes, &acc)
+	fmt.Println(acc)
+	num, err := strconv.Atoi(args[1])
+
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	acc.Balance+=num
-	str:=`{"bank_ID": "`+acc.Bank_ID+`", "balance": `+strconv.Itoa(acc.Balance)+`, "name": "`+acc.Name+`"}`
+	acc.Balance += num
+	str := `{"bank_ID": "` + acc.Bank_ID + `", "balance": ` + strconv.Itoa(acc.Balance) + `, "name": "` + acc.Name + `"}`
 	err = stub.PutState(key, []byte(str))
 
 	if err != nil {
@@ -163,11 +177,10 @@ func (t *SimpleChaincode) deposit(stub shim.ChaincodeStubInterface, args []strin
 		return nil, errors.New(jsonResp)
 	}
 	return nil, nil
-	
+
 }
 
-
-func (t *SimpleChaincode) withdrawal(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
+func (t *SimpleChaincode) withdrawal(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
 
@@ -181,18 +194,17 @@ func (t *SimpleChaincode) withdrawal(stub shim.ChaincodeStubInterface, args []st
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	acc:=account{}
-        json.Unmarshal(valAsbytes,&acc)
-        fmt.Println(acc)
-	num, err:=strconv.Atoi(args[1])
-	
-	
+	acc := account{}
+	json.Unmarshal(valAsbytes, &acc)
+	fmt.Println(acc)
+	num, err := strconv.Atoi(args[1])
+
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	acc.Balance-=num
-	str:=`{"bank_ID": "`+acc.Bank_ID+`", "balance": `+strconv.Itoa(acc.Balance)+`, "name": "`+acc.Name+`"}`
+	acc.Balance -= num
+	str := `{"bank_ID": "` + acc.Bank_ID + `", "balance": ` + strconv.Itoa(acc.Balance) + `, "name": "` + acc.Name + `"}`
 	err = stub.PutState(key, []byte(str))
 
 	if err != nil {
@@ -200,33 +212,27 @@ func (t *SimpleChaincode) withdrawal(stub shim.ChaincodeStubInterface, args []st
 		return nil, errors.New(jsonResp)
 	}
 	return nil, nil
-	
+
 }
 
+func (t *SimpleChaincode) seeAll(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
-func (t *SimpleChaincode) seeAll (stub shim.ChaincodeStubInterface,args []string) ([]byte,error) {
-
-	
 	var index []string
 	var allResults string
-	if len(args)!=0 {
+	if len(args) != 0 {
 		return nil, errors.New("expecting 0 args")
 	}
-	valAsbytes,err:=stub.GetState(indexes)
-	if err!=nil {
-		return nil,err
+	valAsbytes, err := stub.GetState(indexes)
+	if err != nil {
+		return nil, err
 	}
-	json.Unmarshal(valAsbytes,&index)
-	for i:=range index {
-		oneResult,err :=stub.GetState(index[i])
-		if err!=nil {
+	json.Unmarshal(valAsbytes, &index)
+	for i := range index {
+		oneResult, err := stub.GetState(index[i])
+		if err != nil {
 			return nil, errors.New("error!!")
 		}
-		allResults=allResults+string(oneResult[:])
+		allResults = allResults + string(oneResult[:])
 	}
-	return []byte(allResults),nil
+	return []byte(allResults), nil
 }
-
-
-
-
